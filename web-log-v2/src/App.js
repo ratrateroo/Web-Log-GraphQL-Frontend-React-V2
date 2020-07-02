429672b4350b3e2b6f3ce7081f1d04f71a5f229f
Mark <ratrateroo@gmail.com>
2020-07-02 21:18:38 +0800

    managed the starting routes

--- web-log-v2/src/App.js
+++ web-log-v2/src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import {
+   BrowserRouter as Router,
+   Route,
+   Redirect,
+   Switch,
+} from 'react-router-dom';
+
+import Users from './user/pages/Users';
+import NewBlog from './blogs/pages/NewBlog';
 
 const App = () => {
-   return <Router></Router>;
+   return (
+      <Router>
+         <Switch>
+            <Route path="/" exact>
+               <Users />
+            </Route>
+
+            <Route path="/blogs/new" exact>
+               <NewBlog />
+            </Route>
+
+            <Redirect to="/" />
+         </Switch>
+      </Router>
+   );
 };
 
 export default App;
---
+++ web-log-v2/src/blogs/pages/NewBlog.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NewBlog = () => {
+   return <h2>New Blog Works</h2>;
+};
+
+export default NewBlog;
