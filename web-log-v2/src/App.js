c5369bb7e69b808900d54b7667466ebe6a2053f6
Mark <ratrateroo@gmail.com>
2020-08-06 22:12:21 +0800

    added main navigation to the app

--- web-log-v2/src/App.js
+++ web-log-v2/src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import {
-   BrowserRouter as Router,
-   Route,
-   Redirect,
-   Switch,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
 import Users from './user/pages/Users';
 import NewBlog from './blogs/pages/NewBlog';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
    return (
       <Router>
          <Switch>
+            <MainNavigation />
             <Route path="/" exact>
                <Users />
             </Route>
