088632196c4ded479b12d15502f324495d066827
Mark <ratrateroo@gmail.com>
2020-08-08 00:47:53 +0800

    added the main body tot the app

--- web-log-v2/src/App.js
+++ web-log-v2/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-d
 import Users from './user/pages/Users';
 import NewBlog from './blogs/pages/NewBlog';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import MainBody from './shared/components/UIElements/MainBody';
 
 const App = () => {
    return (
@@ -11,7 +12,9 @@ const App = () => {
          <MainNavigation />
          <Switch>
             <Route path="/" exact>
-               <Users />
+               <MainBody>
+                  <Users />
+               </MainBody>
             </Route>
 
             <Route path="/blogs/new" exact>
