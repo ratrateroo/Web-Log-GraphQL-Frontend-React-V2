e3185d348d5579360190288f4d74da71c0ef1068
Mark <ratrateroo@gmail.com>
2020-08-09 22:44:53 +0800

    added dummy info for the blogs page and styling

--- web-log-v2/src/blogs/pages/Blogs.js
+++ web-log-v2/src/blogs/pages/Blogs.js
@@ -12,7 +12,68 @@ const Blogs = props => {
       },
       {
          id: 'b2',
+         image: 'https://source.unsplash.com/1600x900/?mountain',
+         title: 'Wizard of Oz',
+         author: 'Aurora Barnuts',
+      },
+      {
+         id: 'b3',
+         image: 'https://source.unsplash.com/1600x900/?city',
+         title: 'The Throne of the Sphinx',
+         author: 'Aurora Barnuts',
+      },
+      {
+         id: 'b4',
+         image: 'https://source.unsplash.com/1600x900/?space',
+         title: 'Wizard of Oz',
+         author: 'Aurora Barnuts',
+      },
+      {
+         id: 'b5',
          image: 'https://source.unsplash.com/1600x900/?beach',
+         title: 'The Throne of the Sphinx',
+         author: 'Aurora Barnuts',
+      },
+      {
+         id: 'b6',
+         image: 'https://source.unsplash.com/1600x900/?mountain',
+         title: 'Wizard of Oz',
+         author: 'Aurora Barnuts',
+      },
+      {
+         id: 'b7',
+         image: 'https://source.unsplash.com/1600x900/?city',
+         title: 'The Throne of the Sphinx',
+         author: 'Aurora Barnuts',
+      },
+      {
+         id: 'b8',
+         image: 'https://source.unsplash.com/1600x900/?space',
+         title: 'Wizard of Oz',
+         author: 'Aurora Barnuts',
+      },
+
+      {
+         id: 'b9',
+         image: 'https://source.unsplash.com/1600x900/?beach',
+         title: 'The Throne of the Sphinx',
+         author: 'Aurora Barnuts',
+      },
+      {
+         id: 'b10',
+         image: 'https://source.unsplash.com/1600x900/?mountain',
+         title: 'Wizard of Oz',
+         author: 'Aurora Barnuts',
+      },
+      {
+         id: 'b11',
+         image: 'https://source.unsplash.com/1600x900/?city',
+         title: 'The Throne of the Sphinx',
+         author: 'Aurora Barnuts',
+      },
+      {
+         id: 'b12',
+         image: 'https://source.unsplash.com/1600x900/?space',
          title: 'Wizard of Oz',
          author: 'Aurora Barnuts',
       },
