e669723d681923b1ec9d1820604cc2201e84de3e
Mark <ratrateroo@gmail.com>
2020-08-11 07:21:24 +0800

    updated the dummy data for the users

--- web-log-v2/src/user/pages/UserProfile.js
+++ web-log-v2/src/user/pages/UserProfile.js
@@ -3,10 +3,36 @@ import { useParams } from 'react-router-dom';
 
 import MainBody from '../../shared/components/UIElements/MainBody';
 
-useParams();
-
 const UserProfile = props => {
-   const userId = userParams.userId();
+   const USERS = [
+      {
+         id: 'u1',
+         name: 'Mark Tarectecan',
+         image:
+            'https://avatars1.githubusercontent.com/u/35805968?s=400&amp;u=dca0a0c19e5951c9a93d038c97c17e7eea4480d2&amp;v=4" ',
+         blogs: 1,
+         friends: 5,
+      },
+      {
+         id: 'u2',
+         name: 'Mark Leo C. Tarectecan',
+         image:
+            'https://avatars1.githubusercontent.com/u/35805968?s=400&amp;u=dca0a0c19e5951c9a93d038c97c17e7eea4480d2&amp;v=4" ',
+         blogs: 3,
+         friends: 10,
+      },
+      {
+         id: 'u3',
+         name: 'Lei Tarectecan',
+         image:
+            'https://avatars1.githubusercontent.com/u/35805968?s=400&amp;u=dca0a0c19e5951c9a93d038c97c17e7eea4480d2&amp;v=4" ',
+         blogs: 10,
+         friends: 50,
+      },
+   ];
+
+   const userId = useParams.userId;
+
    return (
       <MainBody title={props.title}>
          <UserProfileInfo items={USERS} />
--- web-log-v2/src/user/pages/Users.js
+++ web-log-v2/src/user/pages/Users.js
@@ -12,6 +12,22 @@ const Users = props => {
          blogs: 1,
          friends: 5,
       },
+      {
+         id: 'u2',
+         name: 'Mark Leo C. Tarectecan',
+         image:
+            'https://avatars1.githubusercontent.com/u/35805968?s=400&amp;u=dca0a0c19e5951c9a93d038c97c17e7eea4480d2&amp;v=4" ',
+         blogs: 3,
+         friends: 10,
+      },
+      {
+         id: 'u3',
+         name: 'Lei Tarectecan',
+         image:
+            'https://avatars1.githubusercontent.com/u/35805968?s=400&amp;u=dca0a0c19e5951c9a93d038c97c17e7eea4480d2&amp;v=4" ',
+         blogs: 10,
+         friends: 50,
+      },
    ];
 
    return (
