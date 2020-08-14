14b67ded8dbd3ca6df8c95aea36a0ec3c905e5f2
Mark <ratrateroo@gmail.com>
2020-08-14 23:15:59 +0800

    added footer

---
+++ web-log-v2/src/shared/components/UIElements/MainFooter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './MainFooter.css';
+
+const MainFooter = props => {
+   return (
+      <footer class="footer">
+         <p>Welcome to the Blog</p>
+      </footer>
+   );
+};
+
+export default MainFooter;
