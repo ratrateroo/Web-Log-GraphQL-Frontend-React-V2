d10bbd3ab8cb22031d67a2c504228e75676a660a
Mark <ratrateroo@gmail.com>
2020-08-18 22:49:56 +0800

    fixing button component

--- web-log-v2/src/shared/components/FormElements/Button.js
+++ web-log-v2/src/shared/components/FormElements/Button.js
@@ -5,16 +5,7 @@ import './Button.css';
 
 const Button = props => {
    if (props.href) {
-      return (
-         <a
-            className={`button button--${props.size || 'default'} ${
-               props.inverse && 'button--inverse'
-            } ${props.danger && 'button--danger'}`}
-            href={props.href}
-         >
-            {props.children}
-         </a>
-      );
+      return <a className=".c-form-button__success">{props.children}</a>;
    }
    if (props.to) {
       return (
