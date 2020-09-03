3106f41fd253bee32b9755bd74bcad25cd2d50f4
Mark <ratrateroo@gmail.com>
2020-09-04 01:14:07 +0800

    fixed some bugs in form validation

--- web-log-v2/src/shared/components/FormElements/Button.js
+++ web-log-v2/src/shared/components/FormElements/Button.js
@@ -36,6 +36,7 @@ const Button = props => {
                      `}
          type={props.type}
          onClick={props.onClick}
+         disabled={props.disabled}
       >
          {props.children}
       </button>
--- web-log-v2/src/user/components/UserLoginForm.js
+++ web-log-v2/src/user/components/UserLoginForm.js
@@ -1,13 +1,55 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useReducer } from 'react';
 
 import './UserLoginForm.css';
 import Button from '../../shared/components/FormElements/Button';
 import Input from '../../shared/components/FormElements/Input';
 import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
 
-const UserLoginForm = props => {
-   const usernameInputHandler = useCallback((id, value, isValid) => {}, []);
-   const passwordInputHandler = useCallback((id, value, isValid) => {}, []);
+const formReducer = (state, action) => {
+   switch (action.type) {
+      case 'INPUT_CHANGE':
+         let formIsValid = true;
+         for (const inputId in state.inputs) {
+            if (inputId === action.inputId) {
+               formIsValid = formIsValid && action.isValid;
+            } else {
+               formIsValid = formIsValid && state.inputs[inputId].isValid;
+            }
+         }
+         return {
+            ...state,
+            inputs: {
+               ...state.inputs,
+               [action.inputId]: {
+                  value: action.value,
+                  isValid: action.isValid,
+               },
+            },
+            isValid: formIsValid,
+         };
+      default:
+         return state;
+   }
+};
+
+const UserLoginForm = () => {
+   const [formState, dispatch] = useReducer(formReducer, {
+      inputs: {
+         username: {
+            value: '',
+            isValid: false,
+         },
+         password: {
+            value: '',
+            isValid: false,
+         },
+      },
+      isValid: false,
+   });
+
+   const inputHandler = useCallback((id, value, isValid) => {
+      dispatch({ type: 'INPUT_CHANGE', value: value, isValid: isValid, inputId: id });
+   }, []);
 
    return (
       <div className="c-form">
@@ -20,7 +62,7 @@ const UserLoginForm = props => {
                label="Username:"
                validators={[VALIDATOR_REQUIRE()]}
                errorText="Please enter a valid username."
-               onInput={usernameInputHandler}
+               onInput={inputHandler}
             />
 
             <Input
@@ -31,11 +73,11 @@ const UserLoginForm = props => {
                label="Password:"
                validators={[VALIDATOR_REQUIRE()]}
                errorText="Please enter a valid password."
-               onInput={passwordInputHandler}
+               onInput={inputHandler}
             />
 
             <div className="c-form-button">
-               <Button submit type="submit">
+               <Button submit disabled={!formState.isValid}>
                   Log In
                </Button>
             </div>
