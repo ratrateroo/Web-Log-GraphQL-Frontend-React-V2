afda916ff3c964532ca0fb088f77bf53c0994405
Mark <ratrateroo@gmail.com>
2020-09-05 20:38:50 +0800

    local image used for profile pics

--- web-log-v2/src/user/pages/Users.js
+++ web-log-v2/src/user/pages/Users.js
@@ -2,29 +2,27 @@ import React from 'react';
 
 import UsersList from '../components/UsersList';
 import MainBody from '../../shared/components/UIElements/MainBody';
+import image from '../../Images/profile pic.png';
 const Users = props => {
    const USERS = [
       {
          id: 'u1',
          name: 'Mark Tarectecan',
-         image:
-            'https://avatars1.githubusercontent.com/u/35805968?s=400&amp;u=dca0a0c19e5951c9a93d038c97c17e7eea4480d2&amp;v=4" ',
+         image: image,
          blogs: 1,
          friends: 5,
       },
       {
          id: 'u2',
          name: 'Mark Leo C. Tarectecan',
-         image:
-            'https://avatars1.githubusercontent.com/u/35805968?s=400&amp;u=dca0a0c19e5951c9a93d038c97c17e7eea4480d2&amp;v=4" ',
+         image: image,
          blogs: 3,
          friends: 10,
       },
       {
          id: 'u3',
          name: 'Lei Tarectecan',
-         image:
-            'https://avatars1.githubusercontent.com/u/35805968?s=400&amp;u=dca0a0c19e5951c9a93d038c97c17e7eea4480d2&amp;v=4" ',
+         image: image,
          blogs: 10,
          friends: 50,
       },
