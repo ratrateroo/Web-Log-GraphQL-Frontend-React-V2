dcdaf16b76dabf1a858013cdfc209dcaae23cc07
Mark <ratrateroo@gmail.com>
2020-09-05 22:14:52 +0800

    fixed default styling for ul

--- web-log-v2/src/blogs/pages/Blogs.js
+++ web-log-v2/src/blogs/pages/Blogs.js
@@ -2,79 +2,74 @@ import React from 'react';
 
 import BlogList from '../components/BlogList';
 import MainBody from '../../shared/components/UIElements/MainBody';
+import image from '../../Images/cove view.jpg';
 const Blogs = props => {
    const BLOGS = [
       {
          id: 'b1',
-         image: 'https://source.unsplash.com/1600x900/?beach',
+         image: image,
          title: 'The Throne of the Sphinx',
          author: 'Aurora Barnuts',
       },
       {
          id: 'b2',
-         image: 'https://source.unsplash.com/1600x900/?mountain',
+         image: image,
          title: 'Wizard of Oz',
          author: 'Aurora Barnuts',
       },
       {
          id: 'b3',
-         image: 'https://source.unsplash.com/1600x900/?city',
+         image: image,
          title: 'The Throne of the Sphinx',
          author: 'Aurora Barnuts',
       },
       {
          id: 'b4',
-         image: 'https://source.unsplash.com/1600x900/?space',
+         image: image,
          title: 'Wizard of Oz',
          author: 'Aurora Barnuts',
       },
       {
          id: 'b5',
-         image: 'https://source.unsplash.com/1600x900/?beach',
+         image: image,
          title: 'The Throne of the Sphinx',
          author: 'Aurora Barnuts',
       },
       {
          id: 'b6',
-         image: 'https://source.unsplash.com/1600x900/?mountain',
+         image: image,
          title: 'Wizard of Oz',
          author: 'Aurora Barnuts',
       },
       {
          id: 'b7',
-         image: 'https://source.unsplash.com/1600x900/?city',
+         image: image,
          title: 'The Throne of the Sphinx',
          author: 'Aurora Barnuts',
       },
       {
          id: 'b8',
-         image: 'https://source.unsplash.com/1600x900/?space',
+         image: image,
          title: 'Wizard of Oz',
          author: 'Aurora Barnuts',
       },
 
       {
          id: 'b9',
-         image: 'https://source.unsplash.com/1600x900/?beach',
+         image: image,
          title: 'The Throne of the Sphinx',
          author: 'Aurora Barnuts',
       },
       {
          id: 'b10',
-         image: 'https://source.unsplash.com/1600x900/?mountain',
+         image: image,
          title: 'Wizard of Oz',
          author: 'Aurora Barnuts',
       },
       {
          id: 'b11',
-         image: 'https://source.unsplash.com/1600x900/?city',
+         image: image,
          title: 'The Throne of the Sphinx',
-         author: 'Aurora Barnuts',
-      },
-      {
-         id: 'b12',
-         image: 'https://source.unsplash.com/1600x900/?space',
-         title: 'Wizard of Oz',
          author: 'Aurora Barnuts',
       },
    ];
