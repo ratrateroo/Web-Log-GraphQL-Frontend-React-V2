c52030c3ac47a23c7e20a0dc43f349fc50769cdc
Mark Leo Tarectecan <ratrateroo@gmail.com>
2020-11-29 21:36:19 +0800

    sent login request

--- web-log-v2/src/user/components/UserLoginForm.js
+++ web-log-v2/src/user/components/UserLoginForm.js
@@ -3,11 +3,15 @@ import React, { useState, useContext } from 'react';
 import './UserLoginForm.css';
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../shared/util/validators';
 import { useForm } from '../../shared/hooks/form-hook';
 import { AuthContext } from '../../shared/context/auth-context';
 const UserLoginForm = () => {
    const auth = useContext(AuthContext);
+   const [isLoading, setIsLoading] = useState(false);
+   const [error, setError] = useState();
 
    const [isLoggedInMode, setIsLoggedInMode] = useState(true);
 
@@ -36,41 +40,73 @@ const UserLoginForm = () => {
    const loginSubmitHandler = async event => {
       event.preventDefault();
       console.log(formState.inputs);
-      auth.login();
+      try {
+         setIsLoading(true);
+         const response = await fetch('http://localhost:5000/users/login', {
+            method: 'POST',
+            headers: {
+               'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+               username: formState.inputs.username.value,
+               password: formState.inputs.password.value,
+            }),
+         });
+
+         const responseData = await response.json();
+         if (!response.ok) {
+            throw new Error(responseData.message);
+         }
+
+         console.log(responseData);
+         setIsLoading(false);
+         auth.login();
+      } catch (err) {
+         console.log(err);
+         setIsLoading(false);
+         setError(err.message || 'Something went wrong, please try signing up again.');
+      }
+   };
+
+   const errorHandler = () => {
+      setError(null);
    };
 
    return (
-      <div className="c-form">
-         <form action="" className="c-form__body" onSubmit={loginSubmitHandler}>
-            <Input
-               element="input"
-               id="username"
-               type="text"
-               placeholder="Enter your username here!"
-               label="Username:"
-               validators={[VALIDATOR_REQUIRE()]}
-               errorText="Please enter a valid username."
-               onInput={inputHandler}
-            />
+      <React.Fragment>
+         <ErrorModal error={error} onClear={errorHandler} />
+         <div className="c-form">
+            <form action="" className="c-form__body" onSubmit={loginSubmitHandler}>
+               <Input
+                  element="input"
+                  id="username"
+                  type="text"
+                  placeholder="Enter your username here!"
+                  label="Username:"
+                  validators={[VALIDATOR_REQUIRE()]}
+                  errorText="Please enter a valid username."
+                  onInput={inputHandler}
+               />
 
-            <Input
-               element="input"
-               id="password"
-               type="password"
-               placeholder="Enter your password here!"
-               label="Password:"
-               validators={[VALIDATOR_MINLENGTH(5)]}
-               errorText="Please enter a valid password with a minimum of 5 characters."
-               onInput={inputHandler}
-            />
+               <Input
+                  element="input"
+                  id="password"
+                  type="password"
+                  placeholder="Enter your password here!"
+                  label="Password:"
+                  validators={[VALIDATOR_MINLENGTH(5)]}
+                  errorText="Please enter a valid password with a minimum of 5 characters."
+                  onInput={inputHandler}
+               />
 
-            <div className="c-form-button">
-               <Button submit disabled={!formState.isValid}>
-                  Log In
-               </Button>
-            </div>
-         </form>
-      </div>
+               <div className="c-form-button">
+                  <Button submit disabled={!formState.isValid}>
+                     Log In
+                  </Button>
+               </div>
+            </form>
+         </div>
+      </React.Fragment>
    );
 };
 
