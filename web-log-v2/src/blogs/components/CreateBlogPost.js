455d743fe8f1238694d38093c9bdb13548e7bd38
Mark Leo Tarectecan <ratrateroo@gmail.com>
2020-12-02 02:31:40 +0800

    added blog successfully

--- web-log-v2/src/blogs/components/CreateBlogPost.js
+++ web-log-v2/src/blogs/components/CreateBlogPost.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import './CreateBlogPost.css';
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
+
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../shared/util/validators';
 import { useForm } from '../../shared/hooks/form-hook';
 import { useHttpClient } from '../../shared/hooks/http-hook';
+import { AuthContext } from '../../shared/context/auth-context';
 
 const CreateBlogPost = props => {
+   const auth = useContext(AuthContext);
    const { isLoading, error, sendRequest, clearError } = useHttpClient();
    const [formState, inputHandler] = useForm(
       {
@@ -31,10 +37,6 @@ const CreateBlogPost = props => {
             value: 'testing',
             isValid: true,
          },
-         creator: {
-            value: 'testing',
-            isValid: true,
-         },
          author: {
             value: 'testing',
             isValid: true,
@@ -43,14 +45,41 @@ const CreateBlogPost = props => {
       false
    );
 
-   const createBlogHandler = event => {
+   const history = useHistory();
+
+   const createBlogHandler = async event => {
       event.preventDefault();
-      console.log(formState.inputs);
+      try {
+         await sendRequest(
+            'http://localhost:5000/blogs',
+            'POST',
+            JSON.stringify({
+               title: formState.inputs.title.value,
+               content: formState.inputs.content.value,
+               category: formState.inputs.category.value,
+               edited: formState.inputs.edited.value,
+               created: formState.inputs.created.value,
+               creator: auth.userId,
+               author: formState.inputs.author.value,
+               image: 'testimage',
+               likes: 'asdf',
+               comments: 'asdf',
+            }),
+            {
+               'Content-Type': 'application/json',
+            }
+         );
+         history.push('/');
+      } catch (error) {
+         console.log(error);
+      }
    };
 
    return (
       <div className="c-form-blog">
+         <ErrorModal error={error} onClear={clearError} />
          <form onSubmit={createBlogHandler} className="c-form-blog__body">
+            {isLoading && <LoadingSpinner asOverlay />}
             <Input
                id="title"
                element="input"
