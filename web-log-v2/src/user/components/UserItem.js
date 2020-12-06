92fa430318594ada6dcc1dbe13b70da9b62dcc53
Mark Leo Tarectecan <ratrateroo@gmail.com>
2020-12-06 18:28:32 +0800

    changed navlink component to link

--- web-log-v2/src/user/components/UserItem.js
+++ web-log-v2/src/user/components/UserItem.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import './UserItem.css';
 
 const UserItem = props => {
    return (
       <li className="o-user-list__item" key={props.id}>
-         <NavLink to={`/${props.id}/profile`}>
+         <Link to={`/${props.id}/profile`}>
             <div className="c-user">
                <div className="c-user__avatar">
                   <img className="c-user__avatar-pic" src={props.image} alt={props.image} />
@@ -16,7 +16,7 @@ const UserItem = props => {
                   <h1 className="c-user__fullname">{props.firstname}</h1>
                </div>
             </div>
-         </NavLink>
+         </Link>
       </li>
    );
 };
