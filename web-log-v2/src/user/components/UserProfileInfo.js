c68e9f21584ab2e981d421fd47f9e99e4e7255c5
Mark Leo Tarectecan <ratrateroo@gmail.com>
2020-12-07 21:14:54 +0800

    succesfully get user data from the server

--- web-log-v2/src/user/components/UserProfileInfo.js
+++ web-log-v2/src/user/components/UserProfileInfo.js
@@ -5,49 +5,33 @@ import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 import { useHttpClient } from '../../shared/hooks/http-hook';
 import './UserProfileInfo.css';
 
-// const useQuery = () => {
-//    return new URLSearchParams(useLocation().search);
-// };
-
 const UserProfileInfo = props => {
-   const [parameterValue, setParameterValue] = useState();
-
-   let query = new URLSearchParams(useLocation().search);
    console.log('Start Render'); //=======================================
-
-   const [userId, setUserId] = useState();
+   let query = new URLSearchParams(useLocation().search);
+   const [parameterValue, setParameterValue] = useState(null);
    const [loadedUser, setLoadedUser] = useState();
    const { isLoading, error, sendRequest, clearError } = useHttpClient();
-   const { firstname, image, friends, blogs } = loadedUser;
-   useEffect(() => {
-      const getUidFromQueryString = () => {
-         try {
-            console.log('useEffect 1');
-            setParameterValue(query.get('uid'));
-            console.log('Parameter Value: ', parameterValue);
-         } catch (err) {
-            console.log(err);
-         }
-      };
-      getUidFromQueryString();
-   }, [parameterValue]);
 
    useEffect(
       () => {
-         console.log('useEffect 2');
-
+         setParameterValue(query.get('uid'));
+         console.log('Parameter Value: ', parameterValue);
          //setIsLoading(true);
 
          //use Immediately Invoked Function Expression inside async function
          //do not use async function inside useEffect
          const fetchUsers = async () => {
             try {
-               const responseData = await sendRequest(
-                  `http://localhost:5000/users/profile/${parameterValue}`
-               );
-               console.log('responsedata', responseData.user);
-               //console.log(responseData.user);
-               setLoadedUser(responseData.user);
+               if (parameterValue === null) {
+                  console.log('null value', parameterValue);
+               } else {
+                  const responseData = await sendRequest(
+                     `http://localhost:5000/users/profile/${parameterValue}`
+                  );
+                  //console.log('responsedata', responseData);
+                  //console.log(responseData.user);
+                  setLoadedUser(responseData.user);
+               }
             } catch (err) {
                console.log(err);
             }
@@ -58,21 +42,21 @@ const UserProfileInfo = props => {
       //is coming from outside useEffect
       [sendRequest, parameterValue]
    );
-   console.log('loadeduser', loadedUser);
+   console.log(loadedUser);
    console.log('End Render'); //=======================================
    // console.log(loadedUser);
 
    return (
       <div className="c-user-profile">
          <div className="c-user-profile__image">
-            <img className="c-user-profile__image-pic" src={image} alt={firstname} />
+            <img className="c-user-profile__image-pic" src={parameterValue} alt={parameterValue} />
          </div>
 
          <div className="c-user-profile__info">
-            <h2 className="c-user-profile__info__fullname">{firstname}</h2>
+            <h2 className="c-user-profile__info__fullname">{parameterValue}</h2>
             <ul className="c-user-profile__info__detail-list">
-               <li className="c-user-profile__info__detail-item">Blogs {blogs}</li>
-               <li className="c-user-profile__info__detail-item">Friends {friends}</li>
+               <li className="c-user-profile__info__detail-item">Blogs {parameterValue}</li>
+               <li className="c-user-profile__info__detail-item">Friends {parameterValue}</li>
             </ul>
          </div>
       </div>
