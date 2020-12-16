4fbe4d5fcd1798bb298bd1f2018caf7ace0d995e
Lei <ratrateroo@gmail.com>
2020-12-16 23:23:25 +0800

    stored the userdata to the local storage

--- web-log-v2/src/App.js
+++ web-log-v2/src/App.js
@@ -29,6 +29,13 @@ const App = () => {
    const login = useCallback((uid, token) => {
       setToken(token);
       setUserId(uid);
+      localStorage.setItem(
+         'userData',
+         JSON.stringify({
+            userId: uid,
+            token: token,
+         })
+      );
    }, []);
 
    const logout = useCallback(() => {
