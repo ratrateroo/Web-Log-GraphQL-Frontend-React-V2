29bcf393c5298490351ad9da181128807a53d7f9
Lei <ratrateroo@gmail.com>
2020-12-16 23:35:16 +0800

    accessed the local storage using useEffect

--- web-log-v2/src/App.js
+++ web-log-v2/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
 import Users from './user/pages/Users';
@@ -41,7 +41,15 @@ const App = () => {
    const logout = useCallback(() => {
       setToken(null);
       setUserId(null);
+      localStorage.removeItem('userData');
    }, []);
+
+   useEffect(() => {
+      const storedData = JSON.parse(localStorage.getItem('userData'));
+      if (storedData && storedData.token) {
+         login(storedData.userId, storedData.token);
+      }
+   }, [login]);
 
    let routes;
 
