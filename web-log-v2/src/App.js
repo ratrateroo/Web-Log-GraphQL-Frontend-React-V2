b548d37897f0e2e1727a9e130529589700339017
Lei <ratrateroo@gmail.com>
2020-12-23 22:56:03 +0800

    used the useAuth inside the app

--- web-log-v2/src/App.js
+++ web-log-v2/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
 import Users from './user/pages/Users';
